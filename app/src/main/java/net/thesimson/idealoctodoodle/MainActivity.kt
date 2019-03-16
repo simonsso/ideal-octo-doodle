fd4d3d0fc592fe96026b1bfb7e1c5f8f535d567a
Fredrik Simonsson <simson@thesimson.net>
2019-03-16 21:17:03 +0900

    Project reacted Hello World milestone

--- app/src/main/java/net/thesimson/idealoctodoodle/MainActivity.kt
+++ app/src/main/java/net/thesimson/idealoctodoodle/MainActivity.kt
@@ -1,26 +1,42 @@
 package net.thesimson.idealoctodoodle
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private  fun buttonListener() {
+
+    }
+//    -> {
+//        message.setText("Hello World")
+//        tokyo.setTypeface(Typeface.DEFAULT)
+//        return@OnNavigationItemSelectedListener true
+//
+//    }
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
+                tokyo.setTypeface(Typeface.DEFAULT_BOLD)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 message.setText(R.string.title_dashboard)
+                tokyo.setTypeface(Typeface.MONOSPACE)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 message.setText(R.string.title_notifications)
+                tokyo.setTypeface(Typeface.SANS_SERIF)
                 return@OnNavigationItemSelectedListener true
             }
+
         }
         false
     }
@@ -30,5 +46,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        tokyo.setOnClickListener {
+            message.setText("Hello World")
+            tokyo.setTypeface(Typeface.DEFAULT)
+        }
     }
 }
