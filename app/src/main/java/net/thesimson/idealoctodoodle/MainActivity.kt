f6449ff688176df517e7ce09cd4176fe1fd6835b
Fredrik Simonsson <simson@thesimson.net>
2019-03-18 12:20:53 +0900

    Add open street map

--- app/src/main/java/net/thesimson/idealoctodoodle/MainActivity.kt
+++ app/src/main/java/net/thesimson/idealoctodoodle/MainActivity.kt
@@ -2,22 +2,22 @@ package net.thesimson.idealoctodoodle
 
 import android.graphics.Typeface
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import android.preference.PreferenceManager
+// import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+// import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
 
     private  fun buttonListener() {
 
     }
-//    -> {
-//        message.setText("Hello World")
-//        tokyo.setTypeface(Typeface.DEFAULT)
-//        return@OnNavigationItemSelectedListener true
-//
-//    }
     private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -43,13 +43,50 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var ctx = getApplicationContext()
+
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
+        worldmap.setTileSource(TileSourceFactory.MAPNIK);
+        worldmap.setBuiltInZoomControls(true)
+        worldmap.setMultiTouchControls(true)
+
+        worldmap.controller.setCenter(GeoPoint( 51.507222, -0.1275))
+        // set up my buttons
         tokyo.setOnClickListener {
             message.setText("Hello World")
             tokyo.setTypeface(Typeface.DEFAULT)
+            val point: GeoPoint = GeoPoint(35.683333, 139.683333)
+            worldmap.setExpectedCenter(point)
+
+        }
+        paris.setOnClickListener{
+            message.setText("Paris Calling")
+            val point: GeoPoint = GeoPoint(48.8567,2.3508)
+            worldmap.setExpectedCenter(point)
+        }
+        london.setOnClickListener {
+            message.setText("London")
+            tokyo.setTypeface(Typeface.DEFAULT)
+            val point: GeoPoint = GeoPoint( 51.507222, -0.1275)
+            worldmap.setExpectedCenter(point)
+
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        worldmap.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        worldmap.onPause()
+    }
 }
+
